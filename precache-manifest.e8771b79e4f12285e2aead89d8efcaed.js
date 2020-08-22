b717a17fa4a5cf1b40178cf133d8008694738337
Solomon Lee <squalolee@gmail.com>
2020-08-22 13:38:47 -0400

    Updates

---
+++ precache-manifest.e8771b79e4f12285e2aead89d8efcaed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d41c48528189f456dc557ee6ce88a7a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "51d3fb3887f8be7a102c",
+    "url": "/clicky-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7647f54b2dc53e891760",
+    "url": "/clicky-game/static/js/2.55563123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.55563123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d3fb3887f8be7a102c",
+    "url": "/clicky-game/static/js/main.4a108e02.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
